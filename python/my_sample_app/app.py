8bd09fd738dd1e5309fdee13ff37b73ab288db75
woohwan <whaprk70@naver.com>
2024-01-11 12:33:45 +0900

    #21 Network Connection

--- python/my_sample_app/app.py
+++ python/my_sample_app/app.py
@@ -3,11 +3,10 @@ import os
 
 import aws_cdk as cdk
 
-# from my_sample_app.my_sample_app_stack import MySampleAppStack
 from my_sample_app.my_sample_app_stackL2 import MySampleAppStackL2
 
 app = cdk.App()
-# MySampleAppStack(app, "MySampleAppStack")
+
 MySampleAppStackL2(app, "MySampleAppStackL2")
 
 app.synth()
--- python/my_sample_app/my_sample_app/my_sample_app_stackL2.py
+++ python/my_sample_app/my_sample_app/my_sample_app_stackL2.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     Stack,
+    CfnOutput,
     aws_ec2 as ec2
 )
 from constructs import Construct
@@ -12,3 +13,28 @@ class MySampleAppStackL2(Stack):
         # 1.vpc 생성
         my_vpc = ec2.Vpc(self, 'MyVPC',
                          nat_gateways=0)
+        
+        web_server = ec2.Instance(self, 'WebServer',
+                                  machine_image=ec2.MachineImage.latest_amazon_linux2(),
+                                  instance_type=ec2.InstanceType.of(instance_class=ec2.InstanceClass.T2,
+                                                                    instance_size=ec2.InstanceSize.MICRO),
+                                  vpc=my_vpc,
+                                  vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+                                  user_data_causes_replacement=True,
+                                  ssm_session_permissions=True
+                                  )
+        
+        # Attaching an Elastic IP to keep the DNS name on updates
+        ec2.CfnEIP(self, 'ElasticIP',
+                   instance_id=web_server.instance_id)
+        
+        # Installing package at instance launch
+        web_server.add_user_data('yum update -y',
+                                 'amazon-linux-extras install nginx1',
+                                 'service nginx start')
+        
+        CfnOutput(self, 'WebServerDnsName',
+                  value=web_server.instance_public_dns_name)
+        
+        # Allowing connectins to the web server
+        web_server.connections.allow_from_any_ipv4(ec2.Port.tcp(80), 'Allow HTTP Access from the Internet')
